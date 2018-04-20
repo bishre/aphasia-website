013988c2e1d7fda4a3f77bd1baa91701968f41e9
bishre <bibhorshrestha@gmail.com>
2018-04-20 13:46:59 +0300

    added more sounds

--- home/Listening Game/listeningGame1.js
+++ home/Listening Game/listeningGame1.js
@@ -1,69 +1,65 @@
-// var wordList = ['dog', 'cat', 'cow', 'tiger', 'elephant'];
-var questionArray = [['dog', 'cat', 'cow'], ['cat', 'cow', 'tiger'], ['cow', 'tiger', 'elephant'], ['tiger', 'elephant', 'dog'], ['elephant', 'cat', 'dog']];
 let x = 1;
-let questionN = 1;
-var questionList = [];
-for (let i=0; i<3; i++){
-  let question = questionArray[Math.floor(Math.random()*questionArray.length)];
-  questionList.push(question);
-
+var wordList = ['dog', 'cat', 'cow', 'tiger', 'elephant', 'horse', 'frog', 'monkey', 'bird', 'goat'];
+var newList = [];
+var soundFile = "";
+var correctAnswer = "";
+function createNew(){
+  var wordList = ['dog', 'cat', 'cow', 'tiger', 'elephant', 'horse', 'frog', 'monkey', 'bird', 'goat'];
+  newList = [];
+  soundFile = "";
+  correctAnswer = "";
+  for (let i=0; i<3; i++){
+    var word = wordList[Math.floor(Math.random()*wordList.length)];
+    newList.push(word);
+    wordList.splice(wordList.indexOf(word),1);
+  }
+  correctAnswer = newList[Math.floor(Math.random()*newList.length)];
+  soundFile=  "sounds/"+correctAnswer+".mp3";
+  return newList, correctAnswer, soundFile;
 }
-var questionList = questionArray[Math.floor(Math.random()*5)];
-var correctAnswer = questionList[Math.floor(Math.random()*3)];
-var soundFile=  "sounds/"+correctAnswer+".mp3";
+
+// var questionList = questionArray[Math.floor(Math.random()*5)];
+
 
 function playAudio(){
   document.getElementById('audio').src = soundFile;
 }
 
-var startGame = function(){
-  startQuiz();
-}
+// var startGame = function(){
+//   startQuiz();
+// }
 
 var startQuiz = function(){
-  playAudio();
+  createNew();
+  // playAudio();
   // var questionList = questionArray[Math.floor(Math.random()*5)];
   // var correctAnswer = questionList[Math.floor(Math.random()*3)];
   // var soundFile=  "audio/"+correctAnswer+".mp3";
   // var soundFile="audio/"+wordList[Math.floor(Math.random()*3)]+".mp3";
   // document.getElementById('audio').src = soundFile;
+  document.getElementById('audio').src = soundFile;
   for (let i=0; i<3; i++){
     btn = document.getElementsByClassName('results');
     // btn.setAttribute("id", "word"+x);
-    btn[i].innerHTML=questionList[i];
+    btn[i].innerHTML=newList[i];
     btn[i].onclick = pickWord;
     // document.getElementById('options').appendChild(btn[i]);
     x++;
   }
 }
-var nextQ = function(){
-  document.getElementById('result').innerHTML = '';
-  questionList = questionArray[Math.floor(Math.random()*5)];
-  correctAnswer = questionList[Math.floor(Math.random()*3)];
-  soundFile=  "sounds/"+correctAnswer+".mp3";
-  document.getElementById('audio').src = soundFile;
-    // document.getElementById('audio').src = soundFile;
+function nextQ(){
+  createNew();
+  // questionList = questionArray[Math.floor(Math.random()*5)];
+  // correctAnswer = newList[Math.floor(Math.random()*3)];
+  // soundFile=  "sounds/"+correctAnswer+".mp3";
+  // document.getElementById('audio').src = soundFile;
+    document.getElementById('audio').src = soundFile;
+    document.getElementById('result').innerHTML = "";
     for (let i=0; i<3;i++){
-      // btn = document.createElement('BUTTON');
-      // btn.setAttribute("id", "word"+x);
-      btn[i].innerHTML = questionList[i];
+      btn[i].innerHTML = newList[i];
       btn.onclick = pickWord;
-      console.log(btn.innerHTML);
       x++;
-
     }
-    // for (let y=1;y<4;y++){
-    //   console.log(document.getElementById("word"+y).innerHTML);
-    // }
-
-
-    // for (let y=1;y<4;y++){
-    //   document.getElementById('word'+y).innerHTML = btn[y-1];
-    // }
-}
-
-function skipThis(){
-  nextQ();
 }
 
 
