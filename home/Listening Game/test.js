30d3b3850a85af411e353e9cca048ba8e93e1792
bishre <bibhorshrestha@gmail.com>
2018-04-20 13:47:47 +0300

    removed unnecessary files

--- home/Listening Game/test.js
+++
@@ -1,8 +0,0 @@
-var wordList = ['dog', 'cat', 'cow', 'tiger', 'elephant'];
-var newList = [];
-for (let i=0; i<3; i++){
-  var word = wordList[Math.floor(Math.random()*3)];
-  newList.push(word);
-  wordList.splice(wordList.indexOf(word),1);
-}
-console.log(newList);
