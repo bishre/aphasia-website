828832b317fc7e2b98277cdf89d3225d30b601b0
mochinoko <35254255+mochinoko@users.noreply.github.com>
2018-05-02 10:01:33 +0300

    some changes

--- home/picturequiz/finnish/quiz.js
+++ home/picturequiz/finnish/quiz.js
@@ -1,41 +1,41 @@
 var questions=[
   {
-    question:"Which is a dolphin?",
+    question:"Mikä on delfiini??",
     alternatives:["img/dolphin.jpeg","img/lion.jpeg", "img/bear.jpeg"],
     correct:0
 },
   {
-    question:"Which is a cat?",
+    question:"Mikä on kissa?",
     alternatives:["img/dog.jpg","img/walrus.jpg","img/cat.jpeg"],
     correct:2
   },
   {
-    question:"Which is a lion?",
+    question:"Mikä on leijona?",
     alternatives:["img/bear.jpeg", "img/lion.jpeg", "img/dog.jpg"],
     correct:1
   },
   {
-    question:"Which is a reindeer?",
+    question:"Mikä on poro?",
     alternatives:["img/reindeer.jpg", "img/candle.jpg", "img/tuoli.jpg"],
     correct:0
   },
   {
-    question:"Which is a car?",
+    question:"Mikä on auto?",
     alternatives:["img/joulupukki.jpg", "img/car.jpg", "img/vapp.jpg"],
     correct:1
   },
   {
-    question:"Which is a cow?",
+    question:"Mikä on lehmä?",
     alternatives:["img/airplane.jpg", "img/cow.jpg", "img/ananas.jpg"],
     correct:1
   },
   {
-    question:"Which is straberries?",
+    question:"Mikä on mansikoita?",
     alternatives:["img/dog.jpg", "img/mansikka.jpg", "img/donuts.jpg"],
     correct:1
   },
   {
-    question:"Which is a house?",
+    question:"Mikä on talo?",
     alternatives:["img/koti.jpg", "img/dolphin.jpeg", "img/door.jpg"],
     correct:0
   }
@@ -66,47 +66,21 @@ function choose(index){
 function check(){
   if (pick===questions[id].correct){
     correctAnswer++;
-    document.getElementById('status').innerHTML= "Correct";
+    document.getElementById('status').innerHTML= "Oikea!";
     document.getElementById("status").style.color = "green";
      id++;
      setTimeout(start, 1000*2);
   }else{
-    document.getElementById('status').innerHTML="Incorrect";
+    document.getElementById('status').innerHTML="Väärä";
     document.getElementById("status").style.color = "red";
   }
-
-
-}
-/*var img= ["dolphin.jpeg","lion.jpeg", "bear.jpeg"];
-var pick;
-function start(){
-  shuffle();
-  for (let i = 0; i < img.length; i++) {
-    document.images[i].src=img[i];
-  }
 }
 
- function shuffle(){
-   for (var i = img.length-1; i >0 ; i--) {
-    var j = Math.floor(Math.random()*img.length);
-    var temp = img[i];
-    img[i]=img[j];
-    img[j]=temp;
-   }
-   return img;
-}
-
-function choose(index){
-  pick=index;
-  document.images[index].src=img[index];
-  check();
-}
-
-function check(){
-  if (img[pick]=="dolphin.jpeg"){
-    document.getElementById('status').innerHTML="Correct";
-  }else{
-    document.getElementById('status').innerHTML="Incorrect";
-  }
-
-}*/
+// function toTheNext (){
+//   document.getElementById('status').innerHTML="";
+//   if (pick< questions.length) {
+//     document.getElementById('Quiz').innerHTML="You made it!Congrats";
+//   }else {
+//     document.getElementById('Quiz').innerHTML="You made it!Congrats";
+//   }
+// }
