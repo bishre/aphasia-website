8f3a9cd2aaf1994b033f0bca272af276ac5062dd
mochinoko <35254255+mochinoko@users.noreply.github.com>
2018-05-09 14:44:07 +0300

    picture quiz fi and en
    grammer change
    added go back button

--- home/Finnish version/picturequiz_fi/quiz.js
+++ home/Finnish version/picturequiz_fi/quiz.js
@@ -5,7 +5,7 @@ var questions=[
     correct:0
   },
   {
-    question:"Mikä on kissä?",
+    question:"Mikä on kissa?",
     alternatives:["img/dog.jpg","img/walrus.jpg","img/cat.jpeg"],
     correct:2
   },
@@ -30,7 +30,7 @@ var questions=[
     correct:1
   },
   {
-    question:"Mikä on mansikoita?",
+    question:"Mitkä ovat mansikoita?",
     alternatives:["img/dog.jpg", "img/mansikka.jpg", "img/donuts.jpg"],
     correct:1
   },
@@ -43,39 +43,47 @@ var questions=[
 
 ];
 var correctAnswer=0;
-var id=0;
-var pick;
+var counter=0;
+
 
 function start(){
   document.getElementById('status').innerHTML="";
   document.getElementById('goToGames').style.visibility= "hidden";
-  document.getElementById('question').innerHTML="<h1>"+questions[id].question+"</h1>";
-  document.getElementById('1').src=questions[id].alternatives[0];
-  document.getElementById('2').src=questions[id].alternatives[1];
-  document.getElementById('3').src=questions[id].alternatives[2];
+  document.getElementById('question').innerHTML="<h1>"+questions[counter].question+"</h1>";
+  document.getElementById('1').src=questions[counter].alternatives[0];
+  document.getElementById('2').src=questions[counter].alternatives[1];
+  document.getElementById('3').src=questions[counter].alternatives[2];
 
 }
 
-function choose(index){
-  pick= index;
-  // this.src = questions[id].alternatives[index];
-  // document.images[index].src=questions[id].alternatives[index];
-  check();
+function check(pick){
+  if (pick===questions[counter].correct){
 
-
-}
-
-function check(){
-  if (pick===questions[id].correct){
-    correctAnswer++;
-    document.getElementById('status').innerHTML= "Oikea!!";
+    document.getElementById('status').innerHTML= "Oikein!";
     document.getElementById("status").style.color = "green";
-    id++;
-    setTimeout(start, 1000*1);
 
-
+    setTimeout(nextQ, 1000);
+    counter++;
   }else{
-    document.getElementById('status').innerHTML="Väärä.";
+    document.getElementById('status').innerHTML="Väärin.";
     document.getElementById("status").style.color = "red";
+  }
+}
+
+function nextQ() {
+  if (counter < questions.length) {
+    document.getElementById('status').innerHTML="";
+    document.getElementById('question').innerHTML=questions[counter].question;
+    document.getElementById('1').src=questions[counter].alternatives[0];
+    document.getElementById('2').src=questions[counter].alternatives[1];
+    document.getElementById('3').src=questions[counter].alternatives[2];
+
+  }else {
+    document.getElementById('status').innerHTML="Pääsit kysymysten loppuun. Mennään tekemään lisää muita harjoituksia!";
+    document.getElementById('question').innerHTML = "";
+    document.getElementById('1').style.display = "none";
+    document.getElementById('2').style.display = "none";
+    document.getElementById('3').style.display = "none";
+    document.getElementById('goToGames').style.visibility="visible"
   }
 }
--- home/picturequiz/quiz.js
+++ home/picturequiz/quiz.js
@@ -43,74 +43,46 @@ var questions=[
 
 ];
 var correctAnswer=0;
-var id=0;
-var pick;
+var counter=0;
 
 function start(){
   document.getElementById('status').innerHTML="";
   document.getElementById('goToGames').style.visibility= "hidden";
-  document.getElementById('question').innerHTML="<h1>"+questions[id].question+"</h1>";
-  document.getElementById('1').src=questions[id].alternatives[0];
-  document.getElementById('2').src=questions[id].alternatives[1];
-  document.getElementById('3').src=questions[id].alternatives[2];
+  document.getElementById('question').innerHTML="<h1>"+questions[counter].question+"</h1>";
+  document.getElementById('1').src=questions[counter].alternatives[0];
+  document.getElementById('2').src=questions[counter].alternatives[1];
+  document.getElementById('3').src=questions[counter].alternatives[2];
 
-}
-
-function choose(index){
-  pick= index;
-  // this.src = questions[id].alternatives[index];
-  // document.images[index].src=questions[id].alternatives[index];
-  check();
+  }
 
 
-}
 
-function check(){
-  if (pick===questions[id].correct){
-    correctAnswer++;
+function check(pick){
+  if (pick===questions[counter].correct){
     document.getElementById('status').innerHTML= "Correct";
     document.getElementById("status").style.color = "green";
-    id++;
-    setTimeout(start, 1000*1);
-
-
+    setTimeout(nextQ, 1000);
+    counter++;
   }else{
     document.getElementById('status').innerHTML="Incorrect";
     document.getElementById("status").style.color = "red";
   }
 }
 
+function nextQ(){
 
-/*var img= ["dolphin.jpeg","lion.jpeg", "bear.jpeg"];
-var pick;
-function start(){
-shuffle();
-for (let i = 0; i < img.length; i++) {
-document.images[i].src=img[i];
+  if (counter<questions.length){
+    document.getElementById('status').innerHTML="";
+    document.getElementById('question').innerHTML = questions[counter].question;
+    document.getElementById('1').src=questions[counter].alternatives[0];
+    document.getElementById('2').src=questions[counter].alternatives[1];
+    document.getElementById('3').src=questions[counter].alternatives[2];
+  }else {
+    document.getElementById('status').innerHTML="You've made it to the end!";
+    document.getElementById('question').innerHTML = "";
+    document.getElementById('1').style.display= "none";
+    document.getElementById('2').style.display= "none";
+    document.getElementById('3').style.display= "none";
+    document.getElementById("goToGames").style.visibility = "visible";
+  }
 }
-}
-
-function shuffle(){
-for (var i = img.length-1; i >0 ; i--) {
-var j = Math.floor(Math.random()*img.length);
-var temp = img[i];
-img[i]=img[j];
-img[j]=temp;
-}
-return img;
-}
-
-function choose(index){
-pick=index;
-document.images[index].src=img[index];
-check();
-}
-
-function check(){
-if (img[pick]=="dolphin.jpeg"){
-document.getElementById('status').innerHTML="Correct";
-}else{
-document.getElementById('status').innerHTML="Incorrect";
-}
-
-}*/
